2cd199d5283dba1215177cd131240d2a20b95c32
Vitória Gomes <140112932+vitgomesm@users.noreply.github.com>
2024-10-04 21:41:59 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,65 @@
+#idd1 = 16
+#idd2 = 17
+#idd3 = 18
+
+#if idd1  <= 16:
+#    print('Não é permitido a entrada, pois você é menor de idade')
+
+#elif idd1 >= 18 :
+#    print('Tá liberado a sua entrada')
+
+#if idd2  <= 16:
+#    print('Não é permitido a entrada, pois você é menor de idade')
+#elif idd2==17 :
+#    print('Não é permitido a entrada, porém só falta 1 ano para ser permitido')
+#elif idd2 >= 18 :
+#   print('Tá liberado a sua entrada')
+
+#if idd3  <= 16:
+#    print('Não é permitido a entrada, pois você é menor de idade')
+#elif idd3 ==17 :
+#    print('Não é permitido a entrada, porém só falta 1 ano para ser permitido')
+#elif idd3 >= 18 :
+#    print('Tá liberado a sua entrada')   
+
+
+#if idd1  <= 16 or idd1 >=18:
+#    print('Não é permitido a entrada, pois você é menor de idade') 
+#else : 
+#    print('Tá liberado a sua entrada')   
+
+#if idd2 >=18 :
+#    print('Tá liberado a sua entrada') 
+#else : 
+#    print('Não é permitido a entrada, pois você é menor de idade') 
+
+#if idd3  <= 16 and idd3 >=18:
+#    print('Não é permitido a entrada, pois você é menor de idade') 
+#else : 
+#    print('Tá liberado a sua entrada')   
+  
+
+fruta1='jambo'
+fruta2='maçã'
+fruta3='uva'
+fruta4='maçã'
+num1=67
+num2=12
+num3=12.0
+num4=67.1
+
+print(fruta1==fruta3)
+fruta1=fruta3
+print(fruta1)
+
+print(fruta1==fruta4)
+print(fruta2==fruta4)
+print(fruta3==fruta4)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num2==num3)
+print(num1!=num4)
+
+print(str(num4))
