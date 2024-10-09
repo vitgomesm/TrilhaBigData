7ec59142e3b4e3db16bf8bedcf74023d9a0ea989
Vitória Gomes <140112932+vitgomesm@users.noreply.github.com>
2024-10-08 21:13:59 -0300

    Update Atividade01.py

--- Atividade01.py
+++ Atividade01.py
@@ -1,28 +1,6 @@
 idd1 = 16
 idd2 = 17
 idd3 = 18
-
-if idd1  <= 16:
-    print('Não é permitido a entrada, pois você é menor de idade')
-
-elif idd1 >= 18 :
-    print('Tá liberado a sua entrada')
-
-if idd2  <= 16:
-    print('Não é permitido a entrada, pois você é menor de idade')
-elif idd2==17 :
-    print('Não é permitido a entrada, porém só falta 1 ano para ser permitido')
-elif idd2 >= 18 :
-   print('Tá liberado a sua entrada')
-
-if idd3  <= 16:
-    print('Não é permitido a entrada, pois você é menor de idade')
-elif idd3 ==17 :
-    print('Não é permitido a entrada, porém só falta 1 ano para ser permitido')
-elif idd3 >= 18 :
-    print('Tá liberado a sua entrada')   
-
-
 if idd1  <= 16 or idd1 >=18:
     print('Não é permitido a entrada, pois você é menor de idade') 
 else : 
